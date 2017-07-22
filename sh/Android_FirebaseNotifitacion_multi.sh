b4335141dd50a934da06c25732ab8dea4f786cd5
M.Amatani <amatani@mobile-innovation.jp>
2017-07-22 18:18:29 +0900

    通知用スクリプトバックアップ追加

---
+++ sh/Android_FirebaseNotifitacion_multi.sh
@@ -0,0 +1,20 @@
+API_KEY=AIzaSyCIJzkgwGUKsNv25QvGCfgtpLMffj_EbLY
+TOKEN=dC_E_JKqpBg:APA91bGI-wHdD6NXIKiDsdHChV_isqpa9T_XXD0QdCeRwSVU1KXRnE5_WWLcX76m6aL6Qo1L87RNW7006LC94gIShL1Z_R3wdEDMFnn5G4sRrG7IqbqCKBxRlpCr06cKlCp8vtS0rzpa
+
+##複数端末送信用
+curl \
+  --verbose \
+  --header "Authorization: key=${API_KEY}" \
+  --header "Content-type: application/json" \
+  https://fcm.googleapis.com/fcm/send \
+  -d "{
+  \"registration_ids\": [\"${TOKEN}\"],
+  \"priority\":\"high\",
+  \"notification\": {
+    \"title\": \"Title\",
+    \"body\":\"Message\"
+  }
+}"
+
+
+
